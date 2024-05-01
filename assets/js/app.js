f1f429c75f9361304ec588de093d340bda6c65c4
anurag-512 <anuragsharan51@gmail.com>
2024-05-01 15:59:45 +0530

    section-5 1st slider created

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,39 @@ let menulist = document.querySelector(".nav-items");
 menuicon.addEventListener("click", function () {
     menulist.classList.toggle("show");
 })
+
+
+$('.parent-slide').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    arrow:false,
+    speed: 300,
+    slidesToShow: 1.9,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
