1499fc4ad1bde879b7c47ccc0b0f8aa394802922
anurag-512 <anuragsharan51@gmail.com>
2024-05-03 10:05:47 +0530

    back to top button added

--- assets/js/app.js
+++ assets/js/app.js
@@ -40,3 +40,22 @@ $('.parent-slide').slick({
         }
     ]
 });
+
+
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+function topFunction() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
