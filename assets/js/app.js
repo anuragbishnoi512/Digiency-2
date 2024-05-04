93fa54f73d7a3289c7e6e90a62460d400b41b879
anurag-512 <anuragsharan51@gmail.com>
2024-05-04 11:37:05 +0530

    section-8 image and form added

--- assets/js/app.js
+++ assets/js/app.js
@@ -41,6 +41,41 @@ $('.parent-slide').slick({
     ]
 });
 
+$('.parent-slider').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    arrow:false,
+    speed: 300,
+    slidesToShow: 1.99,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
 
 let mybutton = document.getElementById("myBtn");
 window.onscroll = function () { scrollFunction() };
