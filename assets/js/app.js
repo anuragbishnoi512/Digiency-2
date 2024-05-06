6fef8f3bf0bfd0d52ee02cc5c7c44ba75382f726
anurag-512 <anuragsharan51@gmail.com>
2024-05-06 12:21:30 +0530

    responsive done till section-3

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,8 +18,42 @@ $('.parent-slide').slick({
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+$('.parent-slider').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    arrow:false,
+    speed: 300,
+    slidesToShow:2,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
                 infinite: true,
                 dots: true
             }
@@ -41,40 +75,6 @@ $('.parent-slide').slick({
     ]
 });
 
-$('.parent-slider').slick({
-    dots: true,
-    infinite: true,
-    autoplay: true,
-    arrow:false,
-    speed: 300,
-    slidesToShow: 1.99,
-    slidesToScroll: 1,
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-    ]
-});
 
 
 let mybutton = document.getElementById("myBtn");
